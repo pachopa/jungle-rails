ff4ea7c47814f1fab07730677d2da4f6aa0b2b79
Chris Lee <pachopa0320@gmail.com>
2017-07-07 12:54:13 -0700

    insert two resources one is for users and the other is for login page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
@@ -17,6 +18,23 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+  
+  # GifVault::Application.routes.draw do
+      
+  #     # This route sends requests to our naked url to the *cool* action in the *gif* controller.
+  #     root to: 'gif#cool'
+      
+  #     # I've created a gif controller so I have a page I can secure later. 
+  #     # This is optional (as is the root to: above).
+  #     get '/cool' => 'gif#cool'
+  #     get '/sweet' => 'gif#sweet'
+
+  #     # These routes will be for signup. The first renders a form in the browse, the second will 
+  #     # receive the form and create a user in our database using the data given to us by the user.
+  #     get '/signup' => 'users#new'
+  #     post '/users' => 'users#create'
+
+  # end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
